5bfab8d3f8015bdd28714be6931577c66f791c7d
am-gooru <ashish@gooru.org>
2016-05-02 10:55:57 +0530

    Proper handling of escape of control characters before sending out the response

--- src/main/java/org/gooru/nucleus/handlers/courses/processors/repositories/activejdbc/formatter/SimpleJsonFormatter.java
+++ src/main/java/org/gooru/nucleus/handlers/courses/processors/repositories/activejdbc/formatter/SimpleJsonFormatter.java
@@ -9,7 +9,6 @@ import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.ModelDelegate;
 import org.javalite.common.Convert;
-import org.javalite.common.Escape;
 import org.postgresql.util.PGobject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,18 +22,20 @@ import io.vertx.core.impl.StringEscapeUtils;
  * difference in handling of "jsonb" field type in postgres
  */
 class SimpleJsonFormatter implements JsonFormatter {
+    private static final String JSONB_TYPE = "jsonb";
+    private static final int CAPACITY = 2048;
     private final String[] attributes;
     private final boolean pretty;
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleJsonFormatter.class);
-    
+
     public SimpleJsonFormatter(boolean pretty, List<String> attributes) {
         this.pretty = pretty;
-        this.attributes = (attributes != null && attributes.size() > 0) ? lowerCased(attributes) : null;
+        this.attributes = (attributes != null && !attributes.isEmpty()) ? lowerCased(attributes) : null;
     }
 
     @Override
     public <T extends Model> String toJson(T model) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(CAPACITY);
         String indent = "";
         modelToJson(model, sb, indent);
         return sb.toString();
@@ -42,7 +43,7 @@ class SimpleJsonFormatter implements JsonFormatter {
 
     @Override
     public <T extends Model> String toJson(LazyList<T> modelList) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(CAPACITY);
         sb.append('[');
         if (pretty) {
             sb.append('\n');
@@ -79,46 +80,48 @@ class SimpleJsonFormatter implements JsonFormatter {
         } else {
             names = this.attributes;
         }
-        try {
-            for (int i = 0; i < names.length; i++) {
-                if (i > 0) {
-                    sb.append(',');
-                }
-                if (pretty) {
-                    sb.append("\n  ").append(indent);
-                }
-                String name = names[i];
-                sb.append('"').append(name).append("\":");
-                Object v = model.get(name);
-                if (v == null) {
-                    sb.append("null");
-                } else if (v instanceof Number || v instanceof Boolean) {
-                    sb.append(v);
-                } else if (v instanceof Date) {
-                    sb.append('"').append(Convert.toIsoString((Date) v)).append('"');
-                } else if (v instanceof PGobject && ((PGobject) v).getType().equalsIgnoreCase("jsonb")) {
-                    sb.append(Convert.toString(v));
-                } else {
-                    sb.append('"');
-                    LOGGER.debug("before escaping the data:{}", v);
-                    Escape.json(sb, StringEscapeUtils.escapeJava(Convert.toString(v)));
-                    LOGGER.debug("after escape: {}", sb.toString());
-                    // sb.append(v);
-                    sb.append('"');
-                }
+
+        for (int i = 0; i < names.length; i++) {
+            if (i > 0) {
+                sb.append(',');
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (pretty) {
+                sb.append("\n  ").append(indent);
+            }
+            String name = names[i];
+            sb.append('"').append(name).append("\":");
+            Object v = model.get(name);
+            if (v == null) {
+                sb.append("null");
+            } else if (v instanceof Number || v instanceof Boolean) {
+                sb.append(v);
+            } else if (v instanceof Date) {
+                sb.append('"').append(Convert.toIsoString((Date) v)).append('"');
+            } else if (v instanceof PGobject && ((PGobject) v).getType().equalsIgnoreCase(JSONB_TYPE)) {
+                sb.append(Convert.toString(v));
+            } else if (v instanceof String){
+                sb.append('"');
+                try {
+                    sb.append(StringEscapeUtils.escapeJava(String.valueOf(v)));
+                } catch (Exception e) {
+                    LOGGER
+                        .warn("Failed to parse value of field '{}', will use default string without conversion ", name);
+                    sb.append(Convert.toString(v));
+                }
+                sb.append('"');
+            } else {
+                sb.append('"');
+                sb.append(Convert.toString(v));
+                sb.append('"');
+            }
         }
         if (pretty) {
             sb.append('\n').append(indent);
         }
         sb.append('}');
-
-        LOGGER.debug("## COMPLETE JSON: {}", sb.toString());
     }
 
-    private String[] lowerCased(Collection<String> collection) {
+    private static String[] lowerCased(Collection<String> collection) {
         String[] array = new String[collection.size()];
         int i = 0;
         for (String elem : collection) {
