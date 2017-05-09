0b45de7a5b263f352d81ef90aba9346d99c97b2f
szgooru <sachin@gooru.org>
2017-05-09 12:55:47 +0530

    delete rubric when CUL deleted

---
+++ src/main/java/org/gooru/nucleus/handlers/courses/processors/repositories/activejdbc/entities/AJEntityRubric.java
@@ -0,0 +1,13 @@
+package org.gooru.nucleus.handlers.courses.processors.repositories.activejdbc.entities;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+/**
+ * @author szgooru
+ * Created On: 09-May-2017
+ */
+@Table("rubric")
+public class AJEntityRubric extends Model{
+
+}
